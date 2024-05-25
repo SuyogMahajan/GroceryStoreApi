01730a2ad3beb3d797364aa0abd3027a2fed5957
Suyog Mahajan <86179143+SuyogMahajan@users.noreply.github.com>
2024-05-25 04:43:28 +0000

    k

--- build_files.sh
+++ build_files.sh
@@ -9,9 +9,9 @@ if [ -d "env" ]; then
 fi
 
 # Install dependencies
-python3 -m pip install -r requirements.txt
+python3.10 -m pip install -r requirements.txt
 
 # Collect static files
-python3 manage.py collectstatic --noinput --clear
+python3.10 manage.py collectstatic --noinput --clear
 
 echo "BUILD END"
